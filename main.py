8ad5c237cd9b3f5b565dd07dc304ee58edf3e588
aminyous <amyn.hichri@gmail.com>
2024-01-05 15:34:05 +0100

    Using Try except else finally all together

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-class MyCustomError(Exception):
-    """ This is a doc string for this particular class """
-    def __init__(self, message, code):
-        super().__init__(f"Error code {code}: {message}")
-        self.code = code
+# below is the AI 1.0 code, which works but cannot handle invalid input
+# if the user input something other than an integer at first, the program will break due to a ValueError,
+# caused by calling int() function on an non-integer input result
+#
+# Your task is to use the try-except-else-finally workflow to improve the existing code
+# which can detect an invalid input in the beginning, and prints our an error message: 'Please input integers only.'
+# then proceed to ask the user 'Do you want to play again? (y/N):' like the original function does
+def interact():
+    while True:  # keep looping until user reach break statement
+        try:
+            user_input = int(input('Please input an integer:'))     # turn the user input into an integer
+        except ValueError:
+            print("Please input integers only.")
+            continue
+
+        else:
+            print('{} is {}.'.format(user_input,
+                                     'even' if user_input % 2 == 0 else 'odd'))  # print out the message '{user_input} is {even/odd}.'
+        finally:
+            user_input = input('Do you want to play again? (y/N):')
+            if user_input != 'y':  # quit if the user didn't input `y`
+                print('Goodbye.')
+                break  # break the while loop to quit
 
 
-err = MyCustomError("OUCH! An error happened", 500)
-print(err.__doc__)
+interact()
+
 
 
 
